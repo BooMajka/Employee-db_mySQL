4bfded03932996dbd120eddfd81db623edc02481
BooMajka <visionseers@gmail.com>
2022-01-29 14:05:02 -0600

    logic

--- db/connection.js
+++ db/connection.js
@@ -2,11 +2,12 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: "localhost",
-  port: 3001,
+  port: 3306,
   user: "root",
   password: "",
   database: 'employees'
 });
+
 
 
 module.exports = db;
--- server.js
+++ server.js
@@ -9,7 +9,95 @@ const Department = require("./lib/Department.js");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+let employee = new Employee(db);
+let role = new Role(db);
+let department = new Department(db);
 
+// This is the entry point of the user interface
+function start() {
+
+  db.query( `figlet Employees Database`, (err, res) => {
+    if (err) console.log(err);
+  });
+
+
+  let question = "What would you like to do?";
+  let options = [
+    "View All Employees",
+    "Add Employee",
+    "Remove Employee",
+    "Update Employee Role",
+    "Update Employee Manager",
+    "View All Roles",
+    "Add Role",
+    "Remove Role",
+    "View All Departments",
+    "Add Department",
+    "Remove Department",
+    "Exit",
+  ];
+
+  Inquirer.prompt(
+        {
+            name: "action",
+            type: "list",
+            message: question,
+            choices: options
+        })
+        .then((data) => {
+          switch (data.action) {
+            case "View All Employees":
+              employee.printEmployees();
+              start();
+              break;
+            case "View All Roles":
+              role.printRoles();
+              start();
+              break;
+            case "View All Departments":
+              department.printDepartments();
+              start();
+              break;
+            case "Add Employee":
+              addEmployee();
+              break;
+            case "Add Role":
+              addRole();
+              break;
+            case "Add Department":
+              addDepartment();
+              break;
+            case "Update Employee Role":
+              updateEmployeeRole();
+              break;
+            case "Update Employee Manager":
+              updateEmployeeManager();
+              break;
+            case "Remove Employee":
+              removeEmployee();
+              break;
+            case "Remove Role":
+              removeRole();
+              break;
+            case "Remove Department":
+              removeDepartment();
+              break;
+            case "Exit":
+              console.log(
+                "Thank you for using our HR Employee Tracker. Have a great day."
+              );
+              break;
+            default:
+              console.log(`Action (${data.action}) is not supported.`);
+              start();
+              break;
+          }
+        })
+}
+
+
+
+start();
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
